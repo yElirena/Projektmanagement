117df1b57df2322119d89141204e689a198112b6
yElirena <elirena@posteo.de>
2024-05-06 08:21:08 +0200

    Project-class

---
+++ src/Projektmanagement/Project.java
@@ -0,0 +1,13 @@
+package Projektmanagement;
+
+import java.util.Date;
+
+public class Project {
+	private int projectid;
+	private String acronym;
+	private String title;
+	private String description;
+	private Date startdate;
+	private Date enddate;
+
+}
