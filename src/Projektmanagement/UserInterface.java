27ff338c95812085a7105b6a16f0f62df65366cf
yElirena <elirena@posteo.de>
2024-05-06 11:51:25 +0200

    Grid
    Attempting to use gridlayout

--- src/Projektmanagement/UserInterface.java
+++ src/Projektmanagement/UserInterface.java
@@ -1,5 +1,6 @@
 package Projektmanagement;
 
+import java.awt.GridLayout;
 import javax.swing.*;
 
 public class UserInterface {
@@ -7,22 +8,25 @@ public class UserInterface {
 	public void createWindow() {
 		
 			
-		JFrame frame = new JFrame();
+		JFrame frame = new JFrame("Projekt Management");
 		frame.setSize(800, 800);
-		frame.setTitle("Projekt Management");
+		frame.setLayout(new GridLayout(8, 8));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel inputPanel = new JPanel();
 		JPanel tablePanel = new JPanel();
 		
-		JLabel inputTitle = new JLabel("Formular");
+		JLabel inputTitle = new JLabel("Formular", JLabel.CENTER);
+		inputTitle.setLocation(400, 10);		
 		JLabel firstname = new JLabel("firstname:");
+		firstname.setLocation(300, 550);
 		
 		JTextField fnInput = new JTextField();
-		fnInput.setSize(100, 10);
+		fnInput.setSize(300, 300);
+		fnInput.setLocation(100, 560);
 		
 		
-		JLabel tableTitle = new JLabel("Übersicht");
+		JLabel tableTitle = new JLabel("Übersicht", JLabel.CENTER);
 		
 		
 		inputPanel.add(inputTitle);
@@ -31,6 +35,7 @@ public class UserInterface {
 		tablePanel.add(tableTitle);
 		
 		JSplitPane sl = new JSplitPane(SwingConstants.HORIZONTAL, inputPanel, tablePanel);
+		sl.setDividerLocation(400);
 		
 		frame.add(sl);
 		
